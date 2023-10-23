bafd7602704476e8cd90d301af552f64e0b56c9d
Barun355 <baruntiwary620@gmail.com>
2023-10-23 20:08:41 +0530

    Simple Web App (Router new feature) -------------------- 1. createBrowserRouter 2. loader

---
+++ 07reactRouter/src/components/User/User.jsx
@@ -0,0 +1,14 @@
+import { useParams } from "react-router-dom"
+
+function User() {
+
+    const { id } = useParams()
+
+    return (
+        <div className="bg-gray-600 text-white text-3xl p-4">
+            Uesr: {id}
+        </div>
+    )
+}
+
+export default User
